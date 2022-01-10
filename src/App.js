3f977c779a179386b584d954ea2bb0b9e10fb025
hozzijeong <ghwls0905@naver.com>
2022-01-10 20:54:05 +0900

    Feat: CoinTracker

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-    const EMPTY = "";
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (e) => setToDo(e.target.value);
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (toDo === EMPTY) return;
-        setToDos((cur) => [toDo, ...cur]);
-        setToDo(EMPTY);
-    };
+    const [coins, setCoins] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [usd, setUsd] = useState(0);
+    const [convertVal, setConvertVal] = useState(0);
+    const [curCoinIdx, setCurCoinIdx] = useState(0);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((res) => res.json())
+            .then((data) => {
+                const list = data.filter((element, idx) => idx < 100);
+                setCoins(list);
+                setLoading(false);
+            });
+    }, []);
+    useEffect(() => {
+        if (coins.length === 0) return;
+        const coinPrice = coins[curCoinIdx].quotes.USD.price;
+        setConvertVal(usd / coinPrice);
+    }, [usd, curCoinIdx]);
+    const onUsdChange = (e) => setUsd(e.target.value);
+
+    const onBitChange = (e) => setCurCoinIdx(Number(e.target.value));
     return (
         <div>
-            <h1>ToDos({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    value={toDo}
-                    onChange={onChange}
-                    placeholder="Write ToDo"
-                />
-                <button>add ToDo</button>
-            </form>
-            <hr />
-            <ul>
-                {toDos.map((element, idx) => (
-                    <li key={idx}>{element}</li>
-                ))}
-            </ul>
+            <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+            {loading ? (
+                <h2>Loading</h2>
+            ) : (
+                <div>
+                    <select onChange={onBitChange}>
+                        {coins.map((element, idx) => (
+                            <option key={element.id} value={idx}>
+                                {element.name}({element.symbol}):$
+                                {element.quotes.USD.price}
+                            </option>
+                        ))}
+                    </select>
+                    <hr />
+                    <input type="number" value={usd} onChange={onUsdChange} />
+                    <p>
+                        {convertVal}
+                        {coins[curCoinIdx].symbol}
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
