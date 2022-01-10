9800f4940595b25d9df7c78684fd5ec3c7d24131
hozzijeong <ghwls0905@naver.com>
2022-01-10 16:00:49 +0900

    Feat: useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [text, setText] = useState("");
+    const onClick = () => setCounter((cur) => cur + 1);
+    const onChange = (event) => setText(event.target.value);
+    console.log("Always Renderd");
+    useEffect(() => {
+        console.log("only once");
+    }, []);
+    useEffect(() => {
+        console.log("only counter");
+    }, [counter]);
+    useEffect(() => {
+        console.log("only text");
+    }, [text]);
     return (
         <div>
-            Hello World!
-            <Button text="hello React!" />
+            <input type="text" value={text} onChange={onChange} />
+            <p>{counter}</p>
+            <button onClick={onClick}>add Count</button>
         </div>
     );
 }
